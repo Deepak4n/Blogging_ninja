637bdb13cd04924eb4b9d3b2cf039b9a13203027
Dipak <137324200+Deepak4n@users.noreply.github.com>
2024-09-10 16:12:24 +0530

    Add files via upload

---
+++ Blog-Website-template/static/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import requests
+from post import Post
+
+app = Flask(__name__, static_folder='../static', template_folder='../templates')
+
+response = requests.get("https://api.npoint.io/932f8e756d0f4dfa0f18")
+blog = response.json()
+
+blog_post = []
+for post in blog["blogs"]:
+    post_obj = Post(post["title"], post["subtitle"], post["author"], post["content"])
+    blog_post.append(post_obj)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+    # return send_from_directory(app.static_folder, 'index.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/all-blogs")
+def all_blogs():
+    return render_template("blogs.html", all_blog=blog_post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
---
+++ Blog-Website-template/static/post.py
@@ -0,0 +1,6 @@
+class Post:
+    def __init__(self, title, subtitle, author, content):
+        self.title = title
+        self.subtitle = subtitle
+        self.author = author
+        self.content = content
